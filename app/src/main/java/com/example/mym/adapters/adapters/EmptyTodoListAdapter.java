e4fc33ddd5ec1104b8b79e71f942a54c220e2a05
AnasFajoui <FAJOUI.ANAS@student.emi.ac.ma>
2023-01-23 16:44:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mym/adapters/adapters/EmptyTodoListAdapter.java
@@ -0,0 +1,34 @@
+package com.example.mym.adapters.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mym.R;
+
+public class EmptyTodoListAdapter extends RecyclerView.Adapter<EmptyTodoListAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup
+                                                 parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_empty_item,parent,false);
+        return new ViewHolder(v);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    }
+    @Override
+    public int getItemCount() {
+        return 1;
+    }
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+
+}
